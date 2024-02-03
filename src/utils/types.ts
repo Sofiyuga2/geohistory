70f6ea64d8a46da8233291598399959b4a94e550
Sofiyuga2 <156468942+Sofiyuga2@users.noreply.github.com>
2024-02-03 14:42:40 +0300

    Delete src directory

--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Pioneer {
-    id: number,
-    name: string,
-    description: string,
-    status: number,
-    image: string,
-    date_birthday: number,
-    date_death: number
-}
